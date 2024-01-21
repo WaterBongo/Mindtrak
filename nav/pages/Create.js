c4b4ad77752f5ae2eed677cebae5c230221b6d28
Username1unavailable <107366609+Username1unavailable@users.noreply.github.com>
2024-01-20 17:54:32 -0800

    ee

--- nav/pages/Create.js
+++ nav/pages/Create.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TouchableOpacity, Text, TextInput, Button, StyleSheet, Modal } from 'react-native';
+import { View, TouchableOpacity, Text, TextInput, Button, StyleSheet, Modal, Image } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function Survey() {
@@ -23,10 +23,18 @@ export default function Survey() {
             <Text style={styles.addText}>Journal</Text>
 
             {/* Button to open the form */}
+            <Image 
+          source={require('./assets/book.webp')} // Replace with your image path
+          style={styles.boxImage}
+            />
+            <Text style={styles.journalstartText}>Start Journaling</Text>
+            <Text style={styles.greenText}>Start creating by pressing</Text>
+            <Text style={styles.greenText}>the plus button!</Text>
+
             <TouchableOpacity style={styles.addButton} onPress={() => setModalVisible(true)}>
                 <Ionicons name="add-circle-outline" size={64} />
             </TouchableOpacity>
-            <Text>Journals</Text>
+            <Text style={styles.titleText}>Journals:</Text>
             {/* Modal for the input form */}
             <Modal
                 animationType="slide"
@@ -40,19 +48,19 @@ export default function Survey() {
                         placeholder="Input 1"
                         value={input1}
                         onChangeText={setInput1}
-                        style={styles.input}
+                        style={styles.input1}
                     />
                     <TextInput
                         placeholder="Input 2"
                         value={input2}
                         onChangeText={setInput2}
-                        style={styles.input}
+                        style={styles.input2}
                     />
                     <TextInput
                         placeholder="Input 3"
                         value={input3}
                         onChangeText={setInput3}
-                        style={styles.input}
+                        style={styles.input3}
                     />
                     <Button title="Submit" onPress={handleSubmit} />
                 </View>
@@ -61,9 +69,9 @@ export default function Survey() {
             {/* Displaying recommendations */}
             {recommendations.map((item, index) => (
                 <View key={index} style={styles.recommendationBox}>
-                    <Text>Input 1: {item.input1}</Text>
-                    <Text>Input 2: {item.input2}</Text>
-                    <Text>Input 3: {item.input3}</Text>
+                    <Text>{item.input1}</Text>
+                    <Text>{item.input2}</Text>
+                    <Text>{item.input3}</Text>
                 </View>
             ))}
         </View>
@@ -75,12 +83,28 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        backgroundColor: '#FFFFFF',
     },
     addText: {
         position: 'absolute',
         top: 40,
         left: 20,
+        fontSize: 28,
+    },
+    titleText: {
+        paddingBottom: 40,
+        top: 0, 
+        paddingTop: 0,
         fontSize: 18,
+    },
+    journalstartText: {
+        paddingTop: 10,
+        fontSize: 20,
+        color: '#04406C',
+    },
+    greenText: {
+        fontSize: 15,
+        color: '#365757',
     },
     addButton: {
         marginBottom: 100, // Adjust as needed
@@ -88,22 +112,57 @@ const styles = StyleSheet.create({
     modalView: {
         marginTop: 50,
         marginHorizontal: 20,
-        backgroundColor: 'white',
+        backgroundColor: '#F2F2F2',
         padding: 35,
         alignItems: 'center',
         elevation: 5,
+        borderColor: '#9AC3C9',
+        borderWidth: 2,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        borderBottomLeftRadius: 25,
+        borderBottomRightRadius: 25,
     },
-    input: {
+    input1: {
         height: 40,
         marginBottom: 10,
         borderWidth: 1,
         padding: 10,
         width: '100%',
+        borderColor: '#F2F2F2',
+        fontSize: 20,
+        color: '#04406C',
+        
+    },
+    input2: {
+        height: 40,
+        marginBottom: 10,
+        borderWidth: 1,
+        padding: 10,
+        width: '100%',
+        borderColor: '#F2F2F2',
+        fontSize: 15,
+        color: '#365757',
+        
+    },
+    input3: {
+        height: 40,
+        marginBottom: 10,
+        borderWidth: 1,
+        padding: 10,
+        width: '100%',
+        borderColor: '#F2F2F2',
+        
     },
     recommendationBox: {
         margin: 10,
         padding: 10,
         borderWidth: 1,
-        borderColor: '#ddd',
+        borderColor: '#9AC3C9',
     },
+    boxImage: {
+        width: 50, // Width of the image
+        height: 50, // Height of the image
+
+      },
 });
